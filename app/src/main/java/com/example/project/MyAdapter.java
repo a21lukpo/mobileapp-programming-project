b618a54adcdcbd19d5b7d0157aa5bb69615f60f7
a21lukpo <a21lukpo@student.his.se>
2022-05-22 17:18:23 +0200

    Skapade 3 javafiler (MyAdapter), (JsonData), (ViewHolder). Samt en activity_second layout fil. Jag la in 5 stycken public TextView i Viewholder samt länkade till activity_second genom att använda findViewById. Påbörjade MyAdapter genom att lägga in en lista samt en ArrayList. Skapade även en onCreateViewHolder samt onBindViewHolder. Dessutom la jag till 5 textviews i activity_second

---
+++ app/src/main/java/com/example/project/MyAdapter.java
@@ -0,0 +1,28 @@
+package com.example.project;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyAdapter extends RecyclerView.Adapter<ViewHolder> {
+    private List<JsonData> JsonData;
+    public MyAdapter(ArrayList<JsonData> JsonData){this.JsonData = JsonData;}
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType){
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_main, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position){
+        holder.id=setText(JsonData.get(position).getName());
+    }
+}
---
+++ app/src/main/java/com/example/project/ViewHolder.java
@@ -0,0 +1,25 @@
+package com.example.project;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ViewHolder extends RecyclerView.ViewHolder {
+    public TextView id;
+    public TextView login;
+    public TextView temp1;
+    public TextView temp2;
+    public TextView temp3;
+
+
+    public JsonData(@NonNull View itemView){
+        super(itemView);
+        id = itemView.findViewById(R.id.id);
+        login = itemView.findViewById(R.id.login);
+        temp1 = itemView.findViewById(R.id.temp1);
+        temp2 = itemView.findViewById(R.id.temp2);
+        temp3 = itemView.findViewById(R.id.temp3);
+    }
+}
