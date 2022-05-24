6a98ff31d8a0fc5c14ef8d5ae1cfda2088a0d0f0
a21lukpo <a21lukpo@student.his.se>
2022-05-24 16:44:50 +0200

    Skapade en ny mapp som kallas för asset. Skapade en html fil inuti denna nya mapp. Skapade sedan en public void showInteralWebPage som laddar den html fil som skapades tidigare. Kallar sedan på funktionen i onCreate.

--- app/src/main/java/com/example/project/WebViewActivity.java
+++ app/src/main/java/com/example/project/WebViewActivity.java
@@ -5,17 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebView;
 import android.widget.Button;
 
 public class WebViewActivity extends AppCompatActivity {
 
     private Button back;
+    WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web_view);
 
+        webView = findViewById(R.id.webView);
         back = findViewById(R.id.back);
         back.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -23,5 +26,11 @@ public class WebViewActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        showInternalWebPage();
+    }
+
+    public void showInternalWebPage(){
+        webView.loadUrl("file:///android_asset/webpage.html");
     }
 }
